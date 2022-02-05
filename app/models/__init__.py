0ee1ddc24c28b915d1c746c3be50e06ec01c5811
kielbariso@gmail.com <kielbariso@gmail.com>
2022-02-04 19:05:05 -0600

    added models but only did users relationships

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,6 @@
 from .db import db
 from .user import User
+from .projects import Project
+from .steps import Step
+from .comments import Comment
+from .medias import Media
---
+++ app/models/comments.py
@@ -0,0 +1,12 @@
+from .db import db
+from sqlalchemy import ForeignKey
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey("projects.id"), nullable=False)
+
+    user = db.relationship("User", back_populate="comment")
