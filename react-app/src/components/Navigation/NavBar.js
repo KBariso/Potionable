d4e47188a9fb6ca73a858de99af8a863803b659c
Robert <Robertpopphan@gmail.com>
2022-02-07 18:40:46 -0500

    Made Log In / Sign Up visible only if not signed in. If signed in Log Out becomes visible

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -4,10 +4,17 @@ import LogoutButton from "../auth/LogoutButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
 import "./NavBar.css";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
-  return (
-    <>
+
+  const isUser = useSelector(state => state.session.user)
+
+  let sessionLinks;
+
+  if (isUser) {
+    sessionLinks = (
+      <>
       <nav>
         <ul className="nav-top-options">
           <li>
@@ -20,42 +27,93 @@ const NavBar = () => {
         </ul>
         <ul className="nav-login-options">
           <li>
-            <LoginFormModal />
-          </li>
-          <li>
-            <SignupFormModal />
-          </li>
-          {/* <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li> */}
-          <li>
             <LogoutButton />
           </li>
         </ul>
       </nav>
-      <ul className="nav-bottom-options">
+      <div className="nav-bottom-options">
+        <ul className="nav-bottom-left">
+
         <li>
         <NavLink to="/" exact={true} activeClassName="active">
               Logo
         </NavLink>
         </li>
-        <li>
+        <li className="site-name">
           Potionable
         </li>
         <li>
           Projects
         </li>
+      </ul>
+      <ul className="nav-bottom-right">
+
         <li>
           Publish
         </li>
         <li>
           <input type="text" placeholder="Let's make..." />
         </li>
-
       </ul>
+
+      </div>
     </>
+    )
+  } else {
+    sessionLinks = (
+      <>
+      <nav>
+        <ul className="nav-top-options">
+          <li>
+            <NavLink to="/" exact={true} activeClassName="active">
+              <img src="https://res.cloudinary.com/dd9qejhag/image/upload/v1644269348/Potionable/potion-svgrepo-com_1_va7rug.svg" />
+            </NavLink>
+          </li>
+          <li>PLACE HOLDER1</li>
+          <li>PLACE HOLDER2</li>
+        </ul>
+        <ul className="nav-login-options">
+
+          <li>
+            <LoginFormModal />
+          </li>
+          <li>
+            <SignupFormModal />
+          </li>
+        </ul>
+      </nav>
+      <div className="nav-bottom-options">
+        <ul className="nav-bottom-left">
+
+        <li>
+        <NavLink to="/" exact={true} activeClassName="active">
+              Logo
+        </NavLink>
+        </li>
+        <li className="site-name">
+          Potionable
+        </li>
+        <li>
+          Projects
+        </li>
+      </ul>
+      <ul className="nav-bottom-right">
+
+        <li>
+          Publish
+        </li>
+        <li>
+          <input type="text" placeholder="Let's make..." />
+        </li>
+      </ul>
+
+      </div>
+    </>
+    )
+  }
+
+  return (
+    <>{sessionLinks}</>
   );
 };
 
