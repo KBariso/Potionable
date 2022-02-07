f750a6bce427b71f8a15f93706c689f17e0d598f
Robert <Robertpopphan@gmail.com>
2022-02-07 18:55:30 -0500

    nav bar basic styling and conditionals on displaying navbar info

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -7,114 +7,92 @@ import "./NavBar.css";
 import { useSelector } from "react-redux";
 
 const NavBar = () => {
-
-  const isUser = useSelector(state => state.session.user)
+  const isUser = useSelector((state) => state.session.user);
 
   let sessionLinks;
 
   if (isUser) {
     sessionLinks = (
       <>
-      <nav>
-        <ul className="nav-top-options">
-          <li>
-            <NavLink to="/" exact={true} activeClassName="active">
-              <img src="https://res.cloudinary.com/dd9qejhag/image/upload/v1644269348/Potionable/potion-svgrepo-com_1_va7rug.svg" />
-            </NavLink>
-          </li>
-          <li>PLACE HOLDER1</li>
-          <li>PLACE HOLDER2</li>
-        </ul>
-        <ul className="nav-login-options">
-          <li>
-            <LogoutButton />
-          </li>
-        </ul>
-      </nav>
-      <div className="nav-bottom-options">
-        <ul className="nav-bottom-left">
-
-        <li>
-        <NavLink to="/" exact={true} activeClassName="active">
-              Logo
-        </NavLink>
-        </li>
-        <li className="site-name">
-          Potionable
-        </li>
-        <li>
-          Projects
-        </li>
-      </ul>
-      <ul className="nav-bottom-right">
-
-        <li>
-          Publish
-        </li>
-        <li>
-          <input type="text" placeholder="Let's make..." />
-        </li>
-      </ul>
-
-      </div>
-    </>
-    )
+        <nav>
+          <ul className="nav-top-options">
+            <li>
+              <NavLink to="/" exact={true} activeClassName="active">
+                <img src="https://res.cloudinary.com/dd9qejhag/image/upload/v1644269348/Potionable/potion-svgrepo-com_1_va7rug.svg" alt="potionable logo"/>
+              </NavLink>
+            </li>
+            <li>PLACE HOLDER1</li>
+            <li>PLACE HOLDER2</li>
+          </ul>
+          <ul className="nav-login-options">
+            <li>
+              <LogoutButton />
+            </li>
+          </ul>
+        </nav>
+        <div className="nav-bottom-options">
+          <ul className="nav-bottom-left">
+            <li>
+              <NavLink to="/" exact={true} activeClassName="active">
+                Logo
+              </NavLink>
+            </li>
+            <li className="site-name">Potionable</li>
+            <li>Projects</li>
+          </ul>
+          <ul className="nav-bottom-right">
+            <li>Publish</li>
+            <li>
+              <input type="text" placeholder="Let's make..." />
+            </li>
+          </ul>
+        </div>
+      </>
+    );
   } else {
     sessionLinks = (
       <>
-      <nav>
-        <ul className="nav-top-options">
-          <li>
-            <NavLink to="/" exact={true} activeClassName="active">
-              <img src="https://res.cloudinary.com/dd9qejhag/image/upload/v1644269348/Potionable/potion-svgrepo-com_1_va7rug.svg" />
-            </NavLink>
-          </li>
-          <li>PLACE HOLDER1</li>
-          <li>PLACE HOLDER2</li>
-        </ul>
-        <ul className="nav-login-options">
-
-          <li>
-            <LoginFormModal />
-          </li>
-          <li>
-            <SignupFormModal />
-          </li>
-        </ul>
-      </nav>
-      <div className="nav-bottom-options">
-        <ul className="nav-bottom-left">
-
-        <li>
-        <NavLink to="/" exact={true} activeClassName="active">
-              Logo
-        </NavLink>
-        </li>
-        <li className="site-name">
-          Potionable
-        </li>
-        <li>
-          Projects
-        </li>
-      </ul>
-      <ul className="nav-bottom-right">
-
-        <li>
-          Publish
-        </li>
-        <li>
-          <input type="text" placeholder="Let's make..." />
-        </li>
-      </ul>
-
-      </div>
-    </>
-    )
+        <nav>
+          <ul className="nav-top-options">
+            <li>
+              <NavLink to="/" exact={true} activeClassName="active">
+                <img src="https://res.cloudinary.com/dd9qejhag/image/upload/v1644269348/Potionable/potion-svgrepo-com_1_va7rug.svg" />
+              </NavLink>
+            </li>
+            <li>PLACE HOLDER1</li>
+            <li>PLACE HOLDER2</li>
+          </ul>
+          <ul className="nav-login-options">
+            <li>
+              <LoginFormModal />
+            </li>
+            <li>
+              <SignupFormModal />
+            </li>
+          </ul>
+        </nav>
+        <div className="nav-bottom-options">
+          <ul className="nav-bottom-left">
+            <li>
+              <NavLink to="/" exact={true} activeClassName="active">
+                Logo
+              </NavLink>
+            </li>
+            <li className="site-name">Potionable</li>
+            <li>Projects</li>
+          </ul>
+          <ul className="nav-bottom-right">
+            <li>Publish</li>
+            <li>
+              <input type="text" placeholder="Let's make..." />
+            </li>
+          </ul>
+        </div>
+      </>
+    );
   }
 
-  return (
-    <>{sessionLinks}</>
-  );
+  return <>{sessionLinks}</>;
 };
 
 export default NavBar;
