c8e41f5bcc1f895e7543261ea7979969a8228c47
kielbariso@gmail.com <kielbariso@gmail.com>
2022-02-08 13:15:04 -0600

    starting to create post for comments

--- app/api/comments.py
+++ app/api/comments.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, redirect
+from app.forms.comments import NewComment
 from app.models import db, Comment
 
 
@@ -15,3 +16,19 @@ def get_all_comments():
     comments = [comment.to_dict() for comment in Comment.query.all()]
     # return print(comments, "I AM HERE!!!!!!!!!")
     return jsonify(comments)
+
+
+@comments.route('/', methods=["POST"])
+def comment_form_submit():
+    form = NewComment()
+
+    if form.validate_on_submit():
+        data = form.data
+        new_comment = Comment(comment = data["comment"])
+
+        db.session.add(new_comment)
+        db.session.commit()
+        # return redirect("/")
+
+    else:
+        return "Bad Data"
