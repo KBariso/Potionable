9a6db6157c90c7a0d3c021da4f15185fafee864c
kielbariso@gmail.com <kielbariso@gmail.com>
2022-02-09 18:19:07 -0600

    only comments belonging to the project are shown

--- react-app/src/components/ProjectComments/index.js
+++ react-app/src/components/ProjectComments/index.js
@@ -6,49 +6,48 @@ import { getAllComments } from "../../store/comments";
 import CreateNewComment from "../CommentsForm";
 import DeleteComment from "../DeleteComment";
 
-
 const CommentsList = () => {
   const dispatch = useDispatch();
-  // const user_id = useParams()
-  // console.log(user_id, "I AM COMMENTID!!!!!")
-  // const userComment = commentId.user_id
-  // console.log(userComment, "I AM THE USER COMMENT")
+  const { projectId } = useParams();
+  console.log(projectId, "heehee");
+  // const oneComment = useSelector((state) => state.comments);
+  // // console.log(oneComment[commentId].user_id, "I AM THE ONE COMENNT")
+  // const projectId = oneComment[commentId]
+  // console.log(oneComment, "in the project comments")
+
   useEffect(() => {
     dispatch(getAllComments());
   }, [dispatch]);
 
-  const user = useSelector(state => state.session.user)
-  const userId = user?.id
+  const user = useSelector((state) => state.session.user);
+  const userId = user?.id;
   // console.log(userId)
-//   const commentsObj = useSelector((state) => state.comments);
-//   console.log(commentsObj)
-//   const comments = Object.values(commentsObj);
-//   const projectComments = comments.filter((comment) => comment.projectId === user);
-//   console.log(comments)
+  //   const commentsObj = useSelector((state) => state.comments);
+  //   console.log(commentsObj)
+  //   const comments = Object.values(commentsObj);
+  //   const projectComments = comments.filter((comment) => comment.projectId === user);
+  //   console.log(comments)
   const comments = useSelector((state) => {
-      // console.log(state.comments, "HELLLOOOOO")
-      return Object.values(state.comments)
+    // console.log(state.comments, "HELLLOOOOO")
+    return Object.values(state.comments);
   });
-  console.log(comments, "I AM THE COMMENTS")
-//   if (!user) return <Redirect to="/" />;
+  console.log(comments, "I AM THE COMMENTS");
+  //   if (!user) return <Redirect to="/" />;
 
   return (
-
     <div className="commentsContainer">
       <CreateNewComment />
       <h1 className="commentsHeader">All comments</h1>
-        {comments?.map((comment) => {
-           return (
-            <p>
-              {comment.comment}
-            {comment.user_id === userId ? <NavLink to={`/comments/${comment.id}/edit`}>Edit</NavLink>: null}
+      {comments?.map((comment) => {
+        return (
+          <div>
+            {comment.project_id == projectId ? <p>{comment.comment}</p> : null}
+            {comment.user_id === userId && comment.project_id == projectId ? (
+              <NavLink to={`/comments/${comment.id}/edit`}>Edit</NavLink>) : null}
             <DeleteComment commentId={comment.id} />
-            </p>
-
-           )
-        })}
-
-
+          </div>
+        );
+      })}
     </div>
   );
 };
